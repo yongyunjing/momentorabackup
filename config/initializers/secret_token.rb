5a4154bee900e85a0e02e1c693286d92358296ec
Yong Yun Jing <yong_yun_jing@s2013.sst.edu.sg>
2014-01-14 00:54:34 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Momentora::Application.config.secret_key_base = '06a4cacfb7859e89036aac80313a9661fa295407c29c5d2e30a1d04da7ae638bb1423704f71bf5b43a97a19f442045ebfce5c5e613c682c4fdea008f4104ef12'
---
+++ public/assets/info_controller-cad3edd393f5daf5da05b0afd6388087.rb
@@ -0,0 +1,14 @@
+class InfoController < ApplicationController
+  def index
+  end
+  
+  def about
+  end
+  
+  def feeds
+  end
+  
+  def contact
+  end
+  
+end
